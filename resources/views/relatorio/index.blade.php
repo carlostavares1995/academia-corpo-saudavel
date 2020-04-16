b82f9fe1d1293bc863c727f9f4cb3806633542dc
Carlos Tavares <carlos@versatecnologia.com.br>
2020-04-15 21:18:16 -0300

    Tela de relatório

--- resources/views/relatorio/index.blade.php
+++ resources/views/relatorio/index.blade.php
@@ -11,14 +11,60 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    <p class="mb-0">__Form_Relatório__</p>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label>Tipo Relatório</label>
+                                <select name="tipo_relatorio" class="form-control select2" style="width: 100%;">
+                                    <option value=''>Selecione</option>
+                                    <option value='Alunos Matriculados'>Alunos Matriculados</option>
+                                    <option value='Personal Cadastrados'>Personal Cadastrados </option>
+                                    <option value='Avaliações Cadastradas'>Avaliações Cadastradas </option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label>Data Início</label>
+                                <input name="data_inicio" type="text" class="mask-data form-control" placeholder="DD/MM/AAAA"
+                                       value="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label>Data Fim</label>
+                                <input name="data_fim" type="text" class="mask-data form-control" placeholder="DD/MM/AAAA"
+                                       value="">
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="card-footer text-center">
-                    <a type="button" class="btn btn-primary" href="/relatorio/gerar">
+                    <button type="button" class="gerar btn btn-primary">
                         <i class="fa fa-plus"></i> Gerar Relatório
-                    </a>
+                    </button>
                 </div>
             </div>
         </div>
     </div>
+@stop
+
+@section('js')
+    <script>
+        $(document).ready(function() {
+            $('.mask-data').inputmask("99/99/9999");
+        });
+
+        $(function () {
+            $('.select2').select2();
+
+            $(document).on('click', '.gerar', function() {
+                Swal.fire("Relatório gerado com sucesso!", "", "success");
+            });
+        });
+    </script>
 @stop
